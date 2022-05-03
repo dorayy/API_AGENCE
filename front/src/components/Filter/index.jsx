c62a3aaac232dc48e8ce76bedcaef61bf6f1d329
Louis <louispoulinpro@gmail.com>
2022-05-03 14:22:21 +0200

    Filters

---
+++ front/src/components/Filter/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div className="w-4/5 h-60 rounded-2xl shadow-xl shadow-blue-500/50 bg-white p-4">
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        <h6 className="mb-5 text-lg font-semibold text-black">
+          Affinez votre recherche
+        </h6>
+        <div className="w-full flex justify-between items-center">
+          <select className=" w-32 p-3 rounded-2xl border-2 border-blue-500 text-gray-500">
+            <option className="text-gray-500" selected>
+              Type de bien
+            </option>
+            <option value="Maison" className="text-black">
+              Maison
+            </option>
+            <option value="Appartement" className="text-black">
+              Appartement
+            </option>
+          </select>
+          <select className=" w-32 p-3 rounded-2xl border-2 border-blue-500 text-gray-500">
+            <option className="text-gray-500" selected>
+              Type de contrat
+            </option>
+            <option value="Achat" className="text-black">
+              Achat
+            </option>
+            <option value="Location" className="text-black">
+              Location
+            </option>
+          </select>
+          <input
+            type="number"
+            min="100"
+            max="10000000"
+            placeholder="Budget en €"
+            className=" w-32 p-3 rounded-2xl border-2 border-blue-500"
+          />
+        </div>
+        <button className="h-12 w-2/6 mt-5 text-white rounded-2xl bg-blue-500 shadow-lg shadow-blue-500/50">
+          Rechercher
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default index;
--- front/src/pages/Articles/index.jsx
+++ front/src/pages/Articles/index.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 
+import FilterArticles from "@components/FilterArticles";
 import Footer from "@components/Footer";
 
 const Articles = () => {
   return (
     <>
-      <div className="w-full flex justify-center items-center">
+      <div className="w-full flex flex-col justify-center items-center py-36">
         <div className="w-4/5 flex justify-center items-center">
-          <div className="flex-wrap">
-            <div className="w-2/6"></div>
+          <FilterArticles />
+        </div>
+        <div className="mt-10 w-4/5 flex justify-center items-center">
+          <div className="w-full flex flex-wrap gap-5">
+            <div className="w-32 h-80 bg-white rounded-2xl shadow-lg shadow-blue-500/50 overflow-hidden">
+              <div className="flex justify-center align-center">
+                <div className="w-6/12">
+                  <img
+                    src="https://picsum.photos/200/300?random=1"
+                    alt=""
+                    className="h-full w-full object-contain"
+                  />
+                </div>
+                <div className="w-6/12"></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
