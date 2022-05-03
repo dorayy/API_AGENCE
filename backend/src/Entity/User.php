51833a69095a4395fea51bd2c616f05ff64f67a6
doray <d.balassoupramanien@ecole-ipssi.net>
2022-05-03 16:36:19 +0200

    API/LOGIN + token

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -14,6 +14,7 @@ class User implements JsonSerializable
     private string $username;
     private int $roles;
     private string $password;
+    private ?string $token;
 
     public function jsonSerialize(): mixed
     {
@@ -21,7 +22,116 @@ class User implements JsonSerializable
             "id" => $this->id,
             "email" => $this->email,
             "username" => $this->username,
-            "roles" => $this->roles
+            "roles" => $this->roles,
+            "token" => $this->token
         ];
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     */
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of username
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set the value of username
+     */
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of roles
+     */
+    public function getRoles(): int
+    {
+        return $this->roles;
+    }
+
+    /**
+     * Set the value of roles
+     */
+    public function setRoles(int $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     */
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of token
+     */
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    /**
+     * Set the value of token
+     */
+    public function setToken(?string $token): self
+    {
+        $this->token = $token;
+
+        return $this;
     }
 }
