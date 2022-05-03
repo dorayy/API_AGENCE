f41dd026a6836923351ca9b8e24de271146d5068
Esteban Gonzalez <44523139+frisco78es@users.noreply.github.com>
2022-05-03 14:16:11 +0200

    Routeur update

--- backend/Core/Routeur/Routeur.php
+++ backend/Core/Routeur/Routeur.php
@@ -8,55 +8,56 @@ class Routeur
     {
         if (isset($_SERVER["PATH_INFO"])) {
             $path = explode("/", $_SERVER["PATH_INFO"]);
+            if (isset($path[1]) && $path[1] == "api" && isset($path[2]) && $path[2] == "v2") {
+                if (isset($path[3])) {
+                    $controlleurName = "App\Controlleur\\" . ucfirst($path[3]) . "Controlleur";
+                    $controlleur = new $controlleurName();
 
-            if (isset($path[1])) {
-                $controlleurName = "App\Controlleur\\" . ucfirst($path[1]) . "Controlleur";
-                $controlleur = new $controlleurName();
+                    $id = null;
+                    if (isset($path[4]) && is_numeric($path[4])) {
+                        $id = $path[4];
+                    }
 
-                $id = null;
-                if (isset($path[2]) && is_numeric($path[2])) {
-                    $id = $path[2];
+                    switch ($_SERVER["REQUEST_METHOD"]) {
+                        case 'GET':
+                            if ($id) {
+                                $controlleur->single($id);
+                            } else {
+                                $controlleur->index();
+                            }
+                            break;
+                        case 'POST':
+                            if (!empty($_POST)) {
+                                $controlleur->save($_POST);
+                            } else {
+                                throw new \Exception("Donné manquante pour l'ajout en BDD", 400);
+                            }
+                            break;
+                        case 'PUT':
+                            parse_str(file_get_contents("php://input"), $_PUT);
+                            if ($id && !empty($_PUT)) {
+                                $controlleur->update($id, $_PUT);
+                            } else {
+                                throw new \Exception("Erreur lors de la modification, il mannque des informations", 400);
+                            }
+                            break;
+                        case 'PATCH':
+                            parse_str(file_get_contents("php://input"), $_PATCH);
+                            if ($id && !empty($_PATCH)) {
+                                $controlleur->update($id, $_PATCH);
+                            } else {
+                                throw new \Exception("Erreur lors de la modification, il mannque des informations", 400);
+                            }
+                            break;
+                        case 'DELETE':
+                            if ($id) {
+                                $controlleur->delete($id);
+                            } else {
+                                throw new \Exception("ID manquant", 400);
+                            }
+                            break;
+                    };
                 }
-
-                switch ($_SERVER["REQUEST_METHOD"]) {
-                    case 'GET':
-                        if ($id) {
-                            $controlleur->single($id);
-                        } else {
-                            $controlleur->index();
-                        }
-                        break;
-                    case 'POST':
-                        if (!empty($_POST)) {
-                            $controlleur->save($_POST);
-                        } else {
-                            throw new \Exception("Donné manquante pour l'ajout en BDD", 400);
-                        }
-                        break;
-                    case 'PUT':
-                        parse_str(file_get_contents("php://input"), $_PUT);
-                        if ($id && !empty($_PUT)) {
-                            $controlleur->update($id, $_PUT);
-                        } else {
-                            throw new \Exception("Erreur lors de la modification, il mannque des informations", 400);
-                        }
-                        break;
-                    case 'PATCH':
-                        parse_str(file_get_contents("php://input"), $_PATCH);
-                        if ($id && !empty($_PATCH)) {
-                            $controlleur->update($id, $_PATCH);
-                        } else {
-                            throw new \Exception("Erreur lors de la modification, il mannque des informations", 400);
-                        }
-                        break;
-                    case 'DELETE':
-                        if ($id) {
-                            $controlleur->delete($id);
-                        } else {
-                            throw new \Exception("ID manquant", 400);
-                        }
-                        break;
-                };
             }
         } else {
             throw new \Exception("No route matched", 400);
