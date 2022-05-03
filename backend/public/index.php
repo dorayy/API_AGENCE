b8ed879eafc5fab5018304aa5480188be9751e44
Esteban Gonzalez <44523139+frisco78es@users.noreply.github.com>
2022-05-03 14:09:58 +0200

    Routeur update data

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,49 +1,22 @@
 <?php
 
+use Core\Routeur\Routeur;
+
 define("ROOT", dirname(__DIR__));
 require ROOT . "/vendor/autoload.php";
+require ROOT . "/Core/Routeur/Routeur.php";
 
-// (new CategorieControlleur)->index();
-// (new ArticleControlleur)->index();
-// (new CategorieControlleur)->single(1);
-// (new ArticleControlleur)->single(1);
+// indique quels cliens peuvent se connecté a l'api
+// header("Access-Control-Allow-Origin: http://127.0.0.1:8080");
+header("Access-Control-Allow-Origin: *");
 
-if (isset($_SERVER["PATH_INFO"])) {
-    $path = explode("/", $_SERVER["PATH_INFO"]);
-    if (isset($path[1]) && $path[1] == "api") {
-        if (isset($path[2]) && $path[2] == "v1") {
-            if (isset($path[3])) {
-                $controlleurName = "App\Controlleur\\" . ucfirst($path[3]) . "Controlleur";
-                $controlleur = new $controlleurName();
+// indique les headers autorisés par l'api
+header("Access-Control-Allow-Headers: content-type, token, Authorization");
 
-                if (isset($path[4]) && is_numeric($path[4])) {
+// indique les methodes autorisés pour les requêtes http
+header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE");
 
-                    if (isset($path[5]) && is_string($path[5])) {
-                        $method = $path[5];
-                        $controlleur->$method($path[4]);
-                    } else {
-                        $controlleur->single($path[4]);
-                    }
-                } elseif (isset($path[4]) && is_string($path[4])) {
-                    $method = $path[4];
-                    $controlleur->$method();
-                } else {
-                    $controlleur->index();
-                }
-            }
-        }
-    }
-} else {
-    echo "La donné que vous recherchez n'éxiste pas";
+// indique le temps d'éxistance de ces données
+header("Access-Controle-Max-Age: 172800");
 
-    /**
-     *    $password = "GeeksforGeeks";
-     * $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-     * echo $hashed_password;
-     * if (password_verify($password, $hashed_password)) {
-     *     echo 'Password is valid!';
-     * } else {
-     *    echo 'Invalid password.';
-     * }
-     */
-}
+Routeur::routeur();
